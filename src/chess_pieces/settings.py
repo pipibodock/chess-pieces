b732426dd32465589b89260ba26543ee7d219577
Luiz Fellipe <fellipe.mirandamatos@gmail.com>
2020-12-13 19:56:45 -0300

    Improve compose and settings

--- src/chess_pieces/settings.py
+++ src/chess_pieces/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chess',
         'USER': 'postgres',
-        'PASSWORD': None,
+        'PASSWORD': 'chess',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
